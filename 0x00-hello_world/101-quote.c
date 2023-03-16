297515119b8ca4bdb1a933bd36214fcbe9bac2cc
JamesMathenge <mathengejames64@gmail.com>
2023-03-16 11:59:08 -0700

    task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,9 +6,8 @@
  *
  * Return: Always 0 (Success)
  */
-int main()
+int main(void)
 {
-	char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	write(STDOUT_FILENO, str, sizeof(str));
-	return (0);
-}
+	write(2,
+	and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59)
+	return(1);
