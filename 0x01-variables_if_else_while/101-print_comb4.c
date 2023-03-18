5707b92cbbb5c4d25e27d2f91f3cbc676bd96dd0
JamesMathenge <mathengejames64@gmail.com>
2023-03-18 05:14:15 -0700

    adding spaces in arithmetic expressions

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -13,14 +13,14 @@ int main(void)
 
 	for (a = 0; a < 8; a++)
 	{
-		for (b = a+1; b < 9; b++)
+		for (b = a + 1; b < 9; b++)
 		{
-			for (c = b+1; c < 10; c++)
+			for (c = b + 1; c < 10; c++)
 			{
 				putchar('0' + a);
 				putchar('0' + b);
 				putchar('0' + c);
-				if (a !=7 || b !=8 || c !=9)
+				if (a != 7 || b != 8 || c != 9)
 				{
 				putchar(',');
 				putchar(' ');
