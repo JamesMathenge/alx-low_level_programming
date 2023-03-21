8d1a516fa941cc75ab8aeb4c2e1cf2920df40d03
JamesMathenge <mathengejames64@gmail.com>
2023-03-21 19:28:53 +0000

    task 5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -13,22 +13,16 @@ int print_sign(int n)
 	if (n > 0)
 	{
 		_putchar(43);
-		_putchar(44);
-		_putchar(' ');
 		return (0);
 	}
 	else if (n == 0)
 	{
 		_putchar(48);
-		_putchar(44);
-		_putchar(' ');
 		return (0);
 	}
 	else if (n < 0)
 	{
 		_putchar(45);
-		_putchar(44);
-		_putchar(' ');
 		return (-1);
 	}
 	return (0);
