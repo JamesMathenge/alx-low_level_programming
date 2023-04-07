4349fddd09bf5c63af67413fe51f7cde056f45d7
JamesMathenge <mathengejames64@gmail.com>
2023-04-07 13:59:22 +0000

    task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - entry point of the program
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+		return (0);
+}
---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,6 @@
+#ifndef Main_H
+#define MAIN_H
+
+int _putchar(char c);
+
+#endif
