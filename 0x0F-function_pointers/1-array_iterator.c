46f6d2fec61b68573556ed9098b7ddb4a3053ba9
JamesMathenge <mathengejames64@gmail.com>
2023-04-19 19:07:35 +0000

    removing whitespace

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -11,11 +11,12 @@
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	size_t z = 0;
+
 	if (array == NULL || action == NULL)
 	{
 		return;
 	}
-	
+
 	while (z < size)
 	{
 		action(array[z]);
