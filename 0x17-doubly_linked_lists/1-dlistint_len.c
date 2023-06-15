f12ee637e2f7481b42b1e90fc597a8c7a737bde6
JamesMathenge <mathengejames64@gmail.com>
2023-06-15 17:04:16 +0000

    task 1

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -14,10 +14,10 @@ size_t dlistint_len(const dlistint_t *h)
 {
 	size_t count = 0;
 
-	const dlistint_t *current = h;
+	while (h != NULL)
 	{
-		 count++;
-		 current = current->next;
+		count++;
+		h = h->next;
 	}
 	return (count);
 }
