fe069e9e9aa9f23d2b7846ee9c1f730fad7ee232
JamesMathenge <mathengejames64@gmail.com>
2023-06-19 19:14:29 +0000

    task 0

---
+++ 0x18-dynamic_libraries/putchar.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <unistd.h>
+
+
+/**
+ * _isupper - function that checks for uppercase character
+ * @c: char to check
+ *
+ * Return: 0 or 1
+ */
+
+int _putchar(char c)
+{
+	return write(1, &c, 1);
+}
